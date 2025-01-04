35d215b45f6443a7eff4d69673c8b558ec558b7e
Cyrus Yiu <UnsignedArduino@outlook.com>
2025-01-04 02:02:48 -0500

    Update home page

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # cyrus-yiu
 
+[![Deployed on Cloudflare Pages](https://img.shields.io/badge/Deployed%20on-Cloudflare%20Pages-F38020?logo=cloudflarepages)](https://cyrus-yiu.pages.dev/)
+[![Lint](https://github.com/UnsignedArduino/cyrus-yiu/actions/workflows/eslint.yml/badge.svg)](https://github.com/UnsignedArduino/cyrus-yiu/actions/workflows/eslint.yml)
+[![Format](https://github.com/UnsignedArduino/cyrus-yiu/actions/workflows/prettier.yml/badge.svg?branch=main)](https://github.com/UnsignedArduino/cyrus-yiu/actions/workflows/prettier.yml)
+[![CodeFactor](https://www.codefactor.io/repository/github/unsignedarduino/cyrus-yiu/badge)](https://www.codefactor.io/repository/github/unsignedarduino/cyrus-yiu)
+[![Contribute with Gitpod](https://img.shields.io/badge/Contribute%20with-Gitpod-908a85?logo=gitpod)](https://gitpod.io/#https://github.com/UnsignedArduino/cyrus-yiu)
+
 This is the source code for my portfolio website!
 
 ## Install
@@ -17,3 +23,5 @@ Before commit, make sure to format, (`yarn run format` / `yarn run writeFormat`)
 ## Build and serve
 
 `yarn run build` and `yarn run start`.
+
+This website is currently deployed to Cloudflare Pages at https://cyrus-yiu.pages.dev/.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,43 @@ export default function Home() {
       <p>
         Hello! I{"'"}m Cyrus Yiu, a senior at Woburn Memorial High School. I
         {"'"}m interested in majoring in Computer Engineering, Electrical
-        Engineering, or Computer Science. You can check out my projects{" "}
-        <AutoLink href="/projects">here</AutoLink>!
+        Engineering, or Computer Science. Check out my favorite projects{" "}
+        <AutoLink href="/projects">here</AutoLink> and my GitHub{" "}
+        <AutoLink href="https://github.com/UnsignedArduino">here</AutoLink>!
+      </p>
+      <p>
+        Besides engineering and working on digital and physical projects, I
+        enjoy other activities including competing in the WMHS Math Team (as
+        co-captain) and Science Team. I also teach middle school kids how to
+        code in Python with the WMHS Coding Club. Non-STEM activities include
+        performing in the WMHS Marching and Concert Band (as section leader for
+        pit percussion and clarinets), playing the piano (including recitals and
+        competitions), the occasional online chess game, and skiing!
+      </p>
+      <p>
+        <AutoLink href="https://github.com/UnsignedArduino">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            className="mt-2 me-2"
+            src="https://github-readme-stats.vercel.app/api?username=UnsignedArduino&show_icons=true&hide=contribs&theme=transparent"
+            alt="Cyrus Yiu's GitHub stats"
+          />
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="https://github-readme-stats.vercel.app/api/top-langs/?username=UnsignedArduino&layout=compact&exclude_repo=GIF-converter-showcase&theme=transparent"
+            alt="Cyrus Yiu's top languages"
+          />
+          <br />
+        </AutoLink>
+        <small>
+          <i>
+            Powered by{" "}
+            <AutoLink href="https://github.com/anuraghazra/github-readme-stats">
+              https://github.com/anuraghazra/github-readme-stats
+            </AutoLink>
+            .
+          </i>
+        </small>
       </p>
     </Layout>
   );
