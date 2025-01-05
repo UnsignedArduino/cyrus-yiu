a4756b803ea8bd930e7c9df5a872c8a57f035178
Cyrus Yiu <UnsignedArduino@outlook.com>
2025-01-05 00:04:12 -0500

    Use component for MakeCode Arcade game cards

---
+++ src/components/Cards/ProjectCard/MakeCodeArcadeGameCard.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import ExportedImage from "next-image-export-optimizer";
+import AutoLink from "@/components/Linkable/AutoLink";
+import { StaticImageData } from "next/image";
+
+export default function MakeCodeArcadeGameCard({
+  children,
+  images,
+  title,
+  badge,
+  date,
+  gameURL,
+  links,
+  cardTheme,
+}: {
+  children: React.ReactNode;
+  images: {
+    [image: string]: StaticImageData;
+  }[];
+  title: string;
+  badge?: string;
+  date: string;
+  gameURL: string;
+  links: {
+    [title: string]: string;
+  }[];
+  cardTheme?:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "danger"
+    | "warning"
+    | "info"
+    | "light"
+    | "dark";
+}): React.ReactNode {
+  return (
+    <div
+      className={cardTheme ? `card border-${cardTheme} h-100` : "card h-100"}
+    >
+      {images.map((value) => {
+        const v = Object.entries(value)[0];
+        const imageAlt = v[0];
+        const image = v[1];
+        return (
+          <ExportedImage
+            key={imageAlt}
+            className="card-img-top"
+            src={image}
+            alt={imageAlt}
+            layout="responsive"
+          />
+        );
+      })}
+      <div className="card-body">
+        <h5 className="card-title">
+          {title}
+          {badge && (
+            <>
+              {" "}
+              <small>
+                <span className={`badge text-bg-${cardTheme}`}>{badge}</span>
+              </small>
+            </>
+          )}
+        </h5>
+        <h6 className="card-subtitle mb-2 text-body-secondary">{date}</h6>
+        <p className="card-text">{children}</p>
+        <p className="card-text">
+          <AutoLink
+            href={gameURL}
+            className={
+              cardTheme
+                ? `btn btn-${cardTheme} card-link`
+                : "btn btn-secondary card-link"
+            }
+          >
+            Click to play
+          </AutoLink>
+        </p>
+        {links.map((value) => {
+          const v = Object.entries(value)[0];
+          const text = v[0];
+          const href = v[1];
+          return (
+            <AutoLink key={text} href={href} className="card-link">
+              {text}
+            </AutoLink>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
--- src/pages/projects/makecode-arcade-games.tsx
+++ src/pages/projects/makecode-arcade-games.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/components/Layout";
 import AutoLink from "@/components/Linkable/AutoLink";
 import image0 from "@/assets/makecode-arcade-games/fossil-clicker/0.png";
-import ExportedImage from "next-image-export-optimizer";
+import MakeCodeArcadeGameCard from "@/components/Cards/ProjectCard/MakeCodeArcadeGameCard";
 
 export default function MakeCodeArcadeGames() {
   return (
@@ -20,59 +20,35 @@ export default function MakeCodeArcadeGames() {
       </p>
       <div className="row row-cols-1 row-cols-lg-2 row-cols-xxl-3 g-4 mb-2">
         <div className="col">
-          <div className="card border-primary h-100">
-            <ExportedImage
-              className="card-img-top"
-              src={image0}
-              alt=""
-              layout="responsive"
-            />
-            <div className="card-body">
-              <h5 className="card-title">
-                Fossil Clicker{" "}
-                <small>
-                  <span className="badge text-bg-primary">
-                    #2 Game Jam winner
-                  </span>
-                </small>
-              </h5>
-              <h6 className="card-subtitle mb-2 text-body-secondary">
-                Aug 2024
-              </h6>
-              <p className="card-text">
-                Fossil Clicker is an idle game where you click to dig for
-                fossils! Click the shovel to dig your own fossils, purchase
-                towers to automate the process, and buy upgrades until you have
-                more money than the number of atoms in the observable universe!
-              </p>
-              <p className="card-text">
-                Fossil Clicker won{" "}
-                <AutoLink href="https://arcade.makecode.com/gamejam/prehistoric">
-                  second place in the eighth MakeCode Arcade Game Jam.
-                </AutoLink>
-              </p>
-              <p className="card-text">
-                <AutoLink
-                  href="https://arcade.makecode.com/71136-20766-59616-73262"
-                  className="btn btn-primary card-link"
-                >
-                  Click to play
-                </AutoLink>
-              </p>
-              <AutoLink
-                href="https://forum.makecode.com/t/fossil-clicker-my-prehistoric-game-jam-submission/21524?u=unsignedarduino"
-                className="card-link"
-              >
-                Game forum post
-              </AutoLink>
-              <AutoLink
-                href="https://forum.makecode.com/t/announcement-prehistoric-game-jam-winners/21789?u=unsignedarduino"
-                className="card-link"
-              >
-                Game Jam winners forum post
-              </AutoLink>
-            </div>
-          </div>
+          <MakeCodeArcadeGameCard
+            images={[{ "Fossil Clicker start screen": image0 }]}
+            title="Fossil Clicker"
+            badge="#2 Game Jam winner"
+            date="Aug 2024"
+            gameURL="https://arcade.makecode.com/71136-20766-59616-73262"
+            links={[
+              {
+                "Game forum post":
+                  "https://forum.makecode.com/t/fossil-clicker-my-prehistoric-game-jam-submission/21524?u=unsignedarduino",
+              },
+              {
+                "Game Jam winners forum post":
+                  "https://forum.makecode.com/t/announcement-prehistoric-game-jam-winners/21789?u=unsignedarduino",
+              },
+            ]}
+            cardTheme="info"
+          >
+            Fossil Clicker is an idle game where you click to dig for fossils!
+            Click the shovel to dig your own fossils, purchase towers to
+            automate the process, and buy upgrades until you have more money
+            than the number of atoms in the observable universe!
+            <br />
+            <br />
+            Fossil Clicker won{" "}
+            <AutoLink href="https://arcade.makecode.com/gamejam/prehistoric">
+              second place in the eighth MakeCode Arcade Game Jam.
+            </AutoLink>
+          </MakeCodeArcadeGameCard>
         </div>
       </div>
       <p>More of my games will be added...</p>
