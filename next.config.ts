7c62512026a3f5f917b14a6110cd16830876e3d5
Cyrus Yiu <UnsignedArduino@outlook.com>
2025-01-04 19:13:31 -0500

    Optimize images

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
+import withOptimizedImages from "next-optimized-images";
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig = withOptimizedImages({
   /* config options here */
   output: "export",
   images: { unoptimized: true },
   reactStrictMode: true,
-};
+});
 
 export default nextConfig;
